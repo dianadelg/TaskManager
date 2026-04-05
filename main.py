a1c99d8c5b0ee6e7f15153d1708e0438b09efce6
Diana Del Gaudio <49800866+dianadelg@users.noreply.github.com>
2026-04-05 19:27:11 -0400

    updating main and taskmanager

--- main.py
+++ main.py
@@ -1,1 +1,57 @@
 #CLI
+import taskmanager
+import storage
+
+#view all tasks
+#should this be here or in main?
+def view_tasks():
+    task_list = storage.load_tasks() # returns a dictionary
+    count = 0
+    for task in task_list:
+        print(str(count) + ": " + task)
+        count = count + 1
+
+def main():
+    # Your program logic goes here
+    print("Welcome to Task Manager!")
+    currentAction = -1
+    while(currentAction != 0):
+        print("What would you like to do?")
+        print("Type 1: Add task")
+        print("Type 2: Delete task")
+        print("Type 3: Edit task")
+        print("Type 4: View all current tasks")
+        print("Type 0: Exit Task Manager")
+        currentAction = int(input())
+        
+        if(currentAction == 1):
+            # do add
+            print("Please enter task:")
+            newTask = input()
+            taskmanager.add_task(newTask)
+            print("Task: " + newTask + " added!")
+            print()
+        elif (currentAction == 2):
+            # do delete
+            print("Which task do you want to delete?")
+            view_tasks()
+            deleteTask = int(input())
+            taskmanager.delete_task(deleteTask)
+            print("Task deleted!")
+            print()
+        elif (currentAction == 3):
+            # do edit
+            taskmanager.rename_task()
+            print("Task updated!")
+            print()
+        elif (currentAction == 4):
+            # do list all tasks
+            view_tasks()
+            print()
+        else:
+            # means invalid number entered
+            print("Please enter a valid action")
+            # does this handle if someone enters a sentence or something? 
+
+if __name__ == "__main__":
+    main()
--- taskmanager.py
+++ taskmanager.py
@@ -3,13 +3,42 @@
 # What happens when you complete one?
 # What rules exist?
 # It does NOT care how data is stored
+import storage
 
 #add task
+def add_task(newTask):
+    task_list = storage.load_tasks()
+    task_list.append(newTask)
+    storage.save_tasks(task_list)
+    # AHH add this:
+    # 1. Load existing tasks → task_list
+
+    # 2. If empty:
+    # → id = 1
+
+    # 3. Else:
+    # → find max(taskId)
+    # → id = max + 1
+
+    # 4. Create new task:
+    # {
+    #     taskId: ?
+    #     taskTitle: ?
+    #     taskCompleted: false
+    # } which should be type dictionary. tasks are a list of dictionaries!!!
+
+    # 5. Append that object
+
+    # 6. Save
 
 #update task as complete
+def update_complete(taskId):
+    task_list = storage.load_tasks()
 
 #rename task
+def rename_task(taskId, updatedTask):
+    print("Task renamed!")
 
 #delete task
-
-#view all tasks
+def delete_task(taskId):
+    print("Task deleted!")
